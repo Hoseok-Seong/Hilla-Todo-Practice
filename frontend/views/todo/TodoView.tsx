e1190d849cf089686830e663238ce091568989e2
Hoseok Seong <shsshs0125@naver.com>
2023-06-11 19:08:42 +0900

    Basic Tutorial

--- frontend/views/todo/TodoView.tsx
+++ frontend/views/todo/TodoView.tsx
@@ -11,6 +11,70 @@ export default function TodoView() {
     const empty: Todo = { task: '', done: false };
     const [todos, setTodos] = useState(Array<Todo>());
 
+    useEffect(() => {
+        (async () => {
+            setTodos(await TodoEndpoint.findAll());
+        })();
+
+        return () => { };
+    }, []);
+
+    const formik = useFormik({
+        initialValues: empty,
+        onSubmit: async (value: Todo, { setSubmitting, setErrors }) => {
+            try {
+                const saved = await TodoEndpoint.save(value) ?? value;
+                setTodos([...todos, saved]);
+                formik.resetForm();
+            } catch (e: unknown) {
+                if (e instanceof EndpointValidationError) {
+                    const errors: FormikErrors<Todo> = {}
+                    for (const error of e.validationErrorData) {
+                        if (typeof error.parameterName === 'string' && error.parameterName in empty) {
+                            const key = error.parameterName as (string & keyof Todo);
+                            errors[key] = error.message;
+                        }
+                    }
+                    setErrors(errors);
+                }
+            } finally {
+                setSubmitting(false);
+            }
+        },
+    });
+
+    async function changeStatus(todo: Todo, done: boolean) {
+        const newTodo = { ...todo, done: done };
+        const saved = await TodoEndpoint.save(newTodo) ?? newTodo;
+        setTodos(todos.map(item => item.id === todo.id ? saved : item));
+    }
+
     return (
+        <>
+            <div className="m-m flex items-baseline gap-m">
+                <TextField
+                    name='task'
+                    label="Task"
+                    value={formik.values.task}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleChange}
+                />
+                <Button
+                    theme="primary"
+                    disabled={formik.isSubmitting}
+                    onClick={formik.submitForm}
+                >Add</Button>
+            </div>
+
+            <div className="m-m flex flex-col items-stretch gap-s">
+                {todos.map(todo => (
+                    <Checkbox
+                        key={todo.id}
+                        label={todo.task}
+                        checked={todo.done}
+                        onCheckedChanged={({ detail: { value } }) => changeStatus(todo, value)}/>
+                ))}
+            </div>
+        </>
     );
 }
